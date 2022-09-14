5da4a5aa6e04b26a9c6e2e8de144b864fcba09a1
Zebediah Cupertino <113037863+cadillacechelonhearse@users.noreply.github.com>
2022-09-14 14:24:35 +0000

    finished k26

---
+++ k26.py
@@ -0,0 +1,43 @@
+from stanfordkarel import *
+
+def main():
+    pick_beeper()
+    move()
+    move()
+    turn_left()
+    move()
+    move()
+    turn_left()
+    turn_left()
+    turn_left()
+    move()
+    turn_left()
+    move()
+    move()
+    turn_left()
+    turn_left()
+    turn_left()
+    move()
+    put_beeper()
+    move()
+    turn_left()
+    turn_left()
+    turn_left()
+    move()
+    move()
+    turn_left()
+    move()
+    turn_left()
+    turn_left()
+    turn_left()
+    move()
+    move()
+    turn_left()
+
+    
+    pass
+
+
+if __name__ == "__main__":
+    run_karel_program()
+  
